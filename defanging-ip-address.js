64cd833b6396df4789ecbc5d9d36bb3c99914da5
shivangna <shivangna.kaistha@gmail.com>
2019-07-29 12:18:40 -0400

    complete defanging ip address

---
+++ defanging-ip-address.js
@@ -0,0 +1,7 @@
+//Given a valid (IPv4) IP address, return a defanged version of that IP address.
+//A defanged IP address replaces every period "." with "[.]".
+
+var defangIPaddr = function(address) {
+  return address.replace(/\./g, "[.]");
+};
+console.log(defangIPaddr("1.1.1.1"));
